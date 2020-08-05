51ef4a481ac48f1449686d34e6e3c1c01d25e1bf
DJ Stewart <deshastewart1992@gmail.com>
2020-08-05 09:53:30 -0700

    second commit

--- new-attempt/src/components/Secondary.js
+++ new-attempt/src/components/Secondary.js
@@ -7,7 +7,7 @@ const Secondary = (props) => {
     <div className="container">
     <h4 className="center">Secondary Action</h4><center>
     <img src = "https://steamuserimages-a.akamaihd.net/ugc/94980958203258824/9D5C6AEF168A5416FB47FD1BB85518DDF49C889B/"/>
-    <p>Adding secondary actions to the main action gives a scene more life, and can help to support the main action. A person walking can simultaneously swing their arms or keep them in their pockets, speak or whistle, or express emotions through facial expressions.[24] The important thing about secondary actions is that they emphasize, rather than take attention away from the main action. If the latter is the case, those actions are better left out. For example, during a dramatic movement, facial expressions will often go unnoticed. In these cases, it is better to include them at the beginning and the end of the movement, rather than during.[26]
+    <p>Adding secondary actions to the main action gives a scene more life, and can help to support the main action. A person walking can simultaneously swing their arms or keep them in their pockets, speak or whistle, or express emotions through facial expressions.[24] The important thing about secondary actions is that they emphasize, rather than take attention away from the main action. If the latter is the case, those actions are better left out. For example, during a dramatic movement, facial expressions will often go unnoticed. In these cases, it is better to include them at the beginning and the end of the movement, rather than during.
  </p>
  <div>
 
