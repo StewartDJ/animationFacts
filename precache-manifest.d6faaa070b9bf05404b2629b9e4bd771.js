e85aff7c2b5853a771f20c518fb5e60d8ebf9582
DJ Stewart <deshastewart1992@gmail.com>
2020-08-05 12:50:46 -0700

    Updates

---
+++ precache-manifest.d6faaa070b9bf05404b2629b9e4bd771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aad5f9732c6f1c4e8b09d793aeb235e",
+    "url": "/animationFacts/index.html"
+  },
+  {
+    "revision": "694e69a964b242e6dc15",
+    "url": "/animationFacts/static/css/main.a702efca.chunk.css"
+  },
+  {
+    "revision": "3a965869dc40f978a31e",
+    "url": "/animationFacts/static/js/2.9c55692e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/animationFacts/static/js/2.9c55692e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694e69a964b242e6dc15",
+    "url": "/animationFacts/static/js/main.aa843328.chunk.js"
+  },
+  {
+    "revision": "c7b55c45b49b735774b8",
+    "url": "/animationFacts/static/js/runtime-main.36f424c5.js"
+  }
+]);
